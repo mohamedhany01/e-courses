f16a4733aac46e044afbcb858ff4e097314d3d9a
mohamedhany01 <mohamedhyouns@gmail.com>
2022-11-06 19:39:14 +0200

    Finish lab6 implementation
    - MODIFIED: `pom.xml` javalib path
    - MODIFIED: `pom.xml` to work with java 17
    - MODIFIED: `Makefile` file to run `python` instead `python3`

--- berkeley/cs61b-2021/lab6/capers/CapersRepository.java
+++ berkeley/cs61b-2021/lab6/capers/CapersRepository.java
@@ -1,25 +1,36 @@
 package capers;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
-import static capers.Utils.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
-/** A repository for Capers 
- * @author TODO
- * The structure of a Capers Repository is as follows:
+import static capers.Utils.join;
+
+/**
+ * A repository for Capers
+ * 
+ * @author Mohamed Hany
+ *         The structure of a Capers Repository is as follows:
  *
- * .capers/ -- top level folder for all persistent data in your lab12 folder
- *    - dogs/ -- folder containing all of the persistent data for dogs
- *    - story -- file containing the current story
+ *         .capers/ -- top level folder for all persistent data in your lab12
+ *         folder
+ *         - dogs/ -- folder containing all of the persistent data for dogs
+ *         - story -- file containing the current story
  *
- * TODO: change the above structure if you do something different.
+ *         change the above structure if you do something different.
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = join(CWD, ".capers");
+    // function in Utils
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -27,20 +38,60 @@ public class CapersRepository {
      * Remember: recommended structure (you do not have to follow):
      *
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        File rootDataDir = new File(".capers");
+
+        // Clean files
+        // new File(CAPERS_FOLDER + "/dogs").delete();
+        // new File(CAPERS_FOLDER + "/story").delete();
+        // rootDataDir.delete();
+
+        if (!rootDataDir.exists()) {
+            if (rootDataDir.mkdir()) {
+                File dogsDir = new File(CAPERS_FOLDER, "dogs");
+                if (!dogsDir.exists()) {
+                    dogsDir.mkdir();
+                }
+            }
+        }
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     * 
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        try (FileOutputStream outSource = new FileOutputStream(new File(CAPERS_FOLDER, "story"), true);
+                BufferedOutputStream bufOut = new BufferedOutputStream(outSource);) {
+
+            byte[] storyBytes = text.getBytes();
+            bufOut.write(storyBytes);
+            bufOut.write('\n');
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        try (
+                BufferedReader bufIn = new BufferedReader(new FileReader(join(CAPERS_FOLDER, "story")));) {
+            String storyLine = bufIn.readLine();
+            while (true) {
+                if (storyLine == null) {
+                    break;
+                }
+                System.out.println(storyLine);
+
+                storyLine = bufIn.readLine();
+            }
+
+        } catch (Exception e) {
+            System.err.println(e);
+
+        }
     }
 
     /**
@@ -49,16 +100,19 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog newDog = new Dog(name, breed, age);
+        newDog.saveDog();
     }
 
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
+     * 
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog storedDog = Dog.fromFile(name);
+        storedDog.haveBirthday();
     }
 }
--- berkeley/cs61b-2021/lab6/capers/Dog.java
+++ berkeley/cs61b-2021/lab6/capers/Dog.java
@@ -1,17 +1,24 @@
 package capers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import static capers.Utils.*;
+import static capers.Utils.join;
 
-/** Represents a dog that can be serialized.
- * @author TODO
-*/
-public class Dog { // TODO
+/**
+ * Represents a dog that can be serialized.
+ * 
+ * @author Mohamed Hany
+ */
+public class Dog implements Serializable {
+
+    static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = join(CWD, ".capers", "dogs");
 
     /** Age of dog. */
     private int age;
@@ -22,9 +29,10 @@ public class Dog { // TODO
 
     /**
      * Creates a dog object with the specified parameters.
-     * @param name Name of dog
+     * 
+     * @param name  Name of dog
      * @param breed Breed of dog
-     * @param age Age of dog
+     * @param age   Age of dog
      */
     public Dog(String name, String breed, int age) {
         this.age = age;
@@ -39,8 +47,22 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = new File(DOG_FOLDER, name);
+        Dog tempDog = null;
+        if (dogFile.exists()) {
+            try (ObjectInputStream objOut = new ObjectInputStream(new FileInputStream(dogFile))) {
+                
+                // Read the whole dog info to temp reference variable
+                tempDog = (Dog) objOut.readObject();
+
+                // Delete the old file and higher up the age value
+                tempDog.writeDogToItsUniqueFile(dogFile.getAbsolutePath(),
+                        new Dog(tempDog.name, tempDog.breed, tempDog.age + 1), true);
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+        return tempDog;
     }
 
     /**
@@ -56,14 +78,45 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        System.out.println(this.toString());
+        File dogFile = new File(DOG_FOLDER, name);
+
+        if (!dogFile.exists()) {
+            // Create a new file with same as dog name "unique name"
+            try {
+                if (dogFile.createNewFile()) {
+                    writeDogToItsUniqueFile(dogFile.getAbsolutePath(), this, false);
+                }
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    /* 
+     * Write a dog object to a file
+     */
+    private void writeDogToItsUniqueFile(String createdDogFilePath, Dog dog, boolean deleteFile) {
+        File file = new File(createdDogFilePath);
+
+        if (deleteFile) {
+            file.delete();
+        }
+
+        try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(file))) {
+
+            // Write the whole dog object to the file
+            objOut.writeObject(dog);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 
     @Override
     public String toString() {
         return String.format(
-            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
-            name, breed, age);
+                "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+                name, breed, age);
     }
 
 }
