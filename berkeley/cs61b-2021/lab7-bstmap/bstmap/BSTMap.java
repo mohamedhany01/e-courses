f40f3dc4f582d479c5221c8d2f7c89febbe28d8e
mohamedhany01 <mohamedhyouns@gmail.com>
2023-01-22 20:42:34 +0200

    Add `BSTMap` class

---
+++ berkeley/cs61b-2021/lab7-bstmap/bstmap/BSTMap.java
@@ -0,0 +1,51 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K> , V> implements Map61B{
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
