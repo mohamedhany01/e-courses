81662dafed9c7b985486f494d64d086f0659bbab
mohamedhany01 <mohamedhyouns@gmail.com>
2023-01-30 17:13:54 +0200

    Add gitlet project boilerplate

---
+++ berkeley/cs61b-2021/project2-gitlet/gitlet/Main.java
@@ -0,0 +1,26 @@
+package gitlet;
+
+/** Driver class for Gitlet, a subset of the Git version-control system.
+ *  @author TODO
+ */
+public class Main {
+
+    /** Usage: java gitlet.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+     */
+    public static void main(String[] args) {
+        // TODO: what if args is empty?
+        String firstArg = args[0];
+        switch(firstArg) {
+            case "init":
+                System.out.println("Init");
+                // TODO: handle the `init` command
+                break;
+            case "add":
+                System.out.println("Add");
+                // TODO: handle the `add [filename]` command
+                break;
+            // TODO: FILL THE REST IN
+        }
+    }
+}
