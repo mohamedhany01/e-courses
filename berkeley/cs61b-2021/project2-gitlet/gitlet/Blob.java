083d5e69931cdae322f3ee0c6253f517b1b7c587
mohamedhany01 <mohamedhyouns@gmail.com>
2023-02-21 19:38:22 +0200

    Add `Blob.java` class to be gitlet objects

---
+++ berkeley/cs61b-2021/project2-gitlet/gitlet/Blob.java
@@ -0,0 +1,54 @@
+package gitlet;
+
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/**
+ * Represents a gitlet blob object.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ * @author Mohamed Hany
+ */
+public class Blob implements Serializable {
+
+    private final String fileName;
+    private final String hash;
+    private final String type = "blob";
+
+    private String content;
+
+    public Blob(Path filePath) {
+        this.fileName = setFileName(filePath);
+        this.hash = setHash(filePath);
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    private String setHash(Path path) {
+        byte[] fileContent = Utils.readContents(path.toFile());
+
+        // Set content as well
+        this.content = Arrays.toString(fileContent);
+        return Utils.sha1(fileContent);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    private String setFileName(Path path) {
+        return path.getFileName().toString();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+}
