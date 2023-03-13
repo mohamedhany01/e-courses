1efd254c8e15ebc0dd65ac3b5ccb3766ad3555e9
mohamedhany01 <mohamedhyouns@gmail.com>
2023-03-13 18:22:19 +0200

    Add `TreeTest` unit test to `gitletx`

---
+++ berkeley/cs61b-2021/gitletx/src/test/java/TreeTest.java
@@ -0,0 +1,49 @@
+import org.gitletx.objects.blob.IBlob;
+import org.gitletx.objects.blob.StubBlob;
+import org.gitletx.objects.tree.Tree;
+import org.gitletx.utilities.IUtilitiesWrapper;
+import org.gitletx.utilities.StubUtilitiesWrapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TreeTest {
+
+    @Test
+    public void Tree_getType_returnString() {
+        IUtilitiesWrapper utilities = new StubUtilitiesWrapper();
+        IBlob[] blobs = new StubBlob[]{new StubBlob(), new StubBlob(), new StubBlob()};
+        Tree tree = new Tree(utilities, blobs);
+        String expected = "tree";
+
+        String actual = tree.getType();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Tree_getContent_returnArrayOfString() {
+        IUtilitiesWrapper utilities = new StubUtilitiesWrapper();
+        IBlob[] blobs = new StubBlob[]{new StubBlob(), new StubBlob(), new StubBlob()};
+        Tree tree = new Tree(utilities, blobs);
+
+        String[] expected = new String[]{"hash", "hash", "hash"};
+
+        String[] actual = tree.getContent();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void Tree_getHash_returnString() {
+        IUtilitiesWrapper utilities = new StubUtilitiesWrapper();
+        IBlob[] blobs = new StubBlob[]{new StubBlob(), new StubBlob(), new StubBlob()};
+        Tree tree = new Tree(utilities, blobs);
+        String expected = "sha1";
+
+        String actual = tree.getHash();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
+
