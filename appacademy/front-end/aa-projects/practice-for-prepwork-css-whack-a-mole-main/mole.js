46a52cf97321b99df7a1003aef128164cfb6e65b
mohamedhany01 <mohamedhyouns@gmail.com>
2024-02-11 19:05:23 +0200

    Appacademy | Front-End | practice-for-prepwork-css-whack-a-mole
    Finish main challenge

--- appacademy/front-end/aa-projects/practice-for-prepwork-css-whack-a-mole-main/mole.js
+++ appacademy/front-end/aa-projects/practice-for-prepwork-css-whack-a-mole-main/mole.js
@@ -10,7 +10,7 @@ function popUpRandomMole() {
     return;
   }
 
-  const moleHeads = document.querySelectorAll('.wgs__mole-head');
+  const moleHeads = document.querySelectorAll('.molehill__head');
 
   if (moleHeads.length === 0) {
     return;
@@ -20,8 +20,7 @@ function popUpRandomMole() {
 
   clickable = true;
 
-  // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-  // moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
+  moleHead.classList.remove('molehill__head--hidden', 'molehill__head--whacked');
 
   molesLeft -= 1;
   document.querySelector('.sb__moles').innerHTML = molesLeft;
@@ -31,7 +30,7 @@ function popUpRandomMole() {
 
 function hideMole(mole) {
   clickable = false;
-  mole.classList.add('wgs__mole-head--hidden');
+  mole.classList.add('molehill__head--hidden');
 
   setTimeout(popUpRandomMole, 500);
 }
@@ -39,7 +38,8 @@ function hideMole(mole) {
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(popUpRandomMole, 0);
 
-  const moleHeads = document.querySelectorAll('.wgs__mole-head');
+  const moleHeads = document.querySelectorAll('.molehill__head');
+
   for (let moleHead of moleHeads) {
     moleHead.addEventListener('click', event => {
       if (!clickable) return;
@@ -51,11 +51,9 @@ window.addEventListener('DOMContentLoaded', () => {
       clearTimeout(hideTimeout);
       hideMole(event.target);
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-      // event.target.classList.add('wgs__mole-head--hidden');
+      event.target.classList.add('molehill__head--hidden');
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
-      // event.target.classList.add('wgs__mole-head--whacked');
+      event.target.classList.add('molehill__head--whacked');
     });
   }
 });
