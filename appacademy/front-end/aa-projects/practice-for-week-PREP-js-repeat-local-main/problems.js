a8d4193c3d3dcf70d265546eac5d1d3fa371aaa5
mohamedhany01 <mohamedhyouns@gmail.com>
2024-02-14 18:44:44 +0200

    Appacademy | Front-End | local-javascript-practice
        Finish challenge

--- appacademy/front-end/aa-projects/practice-for-week-PREP-js-repeat-local-main/problems.js
+++ appacademy/front-end/aa-projects/practice-for-week-PREP-js-repeat-local-main/problems.js
@@ -1,23 +1,35 @@
 function divideByThree(num) {
     /* Returns the passed in number argument divided by three. */
-    // Your code here
+    return num / 3;
 };
 
 function averageOfTwo(num1, num2) {
     /* Returns the average of two numbers, num1 and num2. */
-    // Your code here
+    const sum = num1 + num2;
+    const average = sum / 2;
+
+    return average;
 };
 
 function averageOfFour(num1, num2, num3, num4) {
     /* Takes in four numbers. The function should return the average of all of
     the numbers. */
-    // Your code here
+    const sum = num1 + num2 + num3 + num4;
+    const average = sum / 4;
+
+    return average;
 };
 
 function doubler(nums) {
     /* Takes an array of numbers and returns a new array where every element of
     the original array is multiplied by 2. */
-    // Your code here
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums[i] * 2);
+    }
+
+    return result;
 };
 
 function combineArrays(arr1, arr2) {
@@ -25,57 +37,94 @@ function combineArrays(arr1, arr2) {
     a single array. **Hint**: Use the `Array.concat` method but be aware that
     calling this method won't permanently change, also known as **mutate**,
     either array. */
-    // Your code here
+    const result = arr1.concat(arr2);
+
+    return result;
 };
 
 function wordWithinArray(word, arr) {
     /* Takes in both a word and an array of words as arguments and returns a
     boolean that returns true if that string is located inside of the array, or
     false if it does not. Use `Array.indexOf`. */
-    // Your code here
+    for (let i = 0; i < arr.length; i++) {
+        const hasWord = arr[i].indexOf(word);
+
+        if(hasWord > -1) return true;
+    }
+
+    return false;
 };
 
 function echo(str) {
-    /* Takes in a string and returns that string "echo-ized". E.g. 
+    /* Takes in a string and returns that string "echo-ized". E.g.
     echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
     echo("hey"); // => returns "HEY ... hey ... hey"
     echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
-    // Your code here
+    const capitalize = str.toUpperCase();
+    const capitalizeLetter = str[0].toUpperCase() + str.substring(1);
+    const lowercase = str.toLowerCase();
+    const pattern = " ... ";
+
+    return capitalize + pattern + capitalizeLetter + pattern + lowercase;
 };
 
 function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
-    // Your code here
+    const result = [];
+
+    for (let i = 0; i < max; i++) {
+        const divisibleBy3 = i % 3;
+        const divisibleBy5 = i % 5;
+
+        if(divisibleBy3 === 0 && divisibleBy5 === 0) continue;
+
+        if(divisibleBy3 === 0) result.push(i);
+
+        if(divisibleBy5 === 0) result.push(i);
+    }
+
+    return result;
 };
 
 function hello(name) {
     /* Takes in a string name and returns a string saying "Hello, " to that name. */
-    // Your code here
+    const result = `Hello, ${name}`;
+
+    return result;
 };
 
 function goodbye(name) {
     /* Takes in a string name and returns a string saying "Bye, " to that name. */
-    // Your code here
+    const result = `Bye, ${name}`;
+
+    return result;
 };
 
 function isFive(num) {
     /* Takes in a number, num, and returns `true` if a number is equal to 5 and
     `false` if it is not. */
-    // Your code here
+
+    return num === 5;
 };
 
 function isOdd(num) {
     /* Takes in a number and returns `true` if the number is odd and returns
     `false` otherwise. Try writing this with and without `if` statements */
-    // Your code here
+
+    return num % 2 > 0;
 };
 
 function isSubString(searchString, subString) {
     /* Takes in two strings, `searchString` and `subString`. Should return
     `true` if `subString` is a part of the`searchString`, regardless of upper
     or lower case, and `false` if otherwise. */
-    // Your code here
+
+    const cleanedSub = subString.toLowerCase();
+    const cleanedSearch = searchString.toLowerCase();
+    const result = cleanedSearch.indexOf(cleanedSub);
+
+    return result > -1;
 };
 
 function aCounter(word) {
@@ -84,7 +133,7 @@ function aCounter(word) {
     function to use a `for` loop instead of the `while` loop it is currently
     using. */
 
-    /* 
+    /*
     let index = 0;
     let count = 0;
     while (index < word.length) {
@@ -96,7 +145,15 @@ function aCounter(word) {
     }
     return count;
     */
-    // Your code here
+
+    let counter = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        const currentChar = word[i];
+        if(currentChar === 'a' || currentChar === 'A') counter++;
+    }
+
+    return counter;
 };
 
 module.exports = {
@@ -113,5 +170,5 @@ module.exports = {
     isFive,
     isOdd,
     isSubString,
-    aCounter   
+    aCounter
 }
