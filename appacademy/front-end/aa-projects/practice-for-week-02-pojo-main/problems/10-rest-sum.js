72272704a529f107ececb0509f606e301b146404
mohamedhany01 <mohamedhyouns@gmail.com>
2024-02-23 14:59:37 +0200

    Appacademy | Front-End | practice-for-week-02-pojo
     - Starter files

---
+++ appacademy/front-end/aa-projects/practice-for-week-02-pojo-main/problems/10-rest-sum.js
@@ -0,0 +1,17 @@
+/***********************************************************************
+Write a function named `restSum` that accepts all incoming parameters and sums them.
+
+**Hint**: Use rest parameter syntax!
+
+Examples:
+restSum(3,5,6); // => 14
+restSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+restSum(0); // => 0
+***********************************************************************/
+
+function restSum(...otherNums) {
+  // Your code here
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = restSum;
