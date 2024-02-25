d1f925d1eb2a3d8dee154e5fddc5bd22f8440c2a
mohamedhany01 <mohamedhyouns@gmail.com>
2024-02-25 17:59:22 +0200

    Appacademy | Front-End | scope-project
    - Starter files

---
+++ appacademy/front-end/aa-projects/practice-for-week-02-scope-problems-without-context-main/problems/05-arrow-mirror-array.js
@@ -0,0 +1,19 @@
+/***********************************************************************
+Write a function `mirrorArray(array)` that takes in an array as an
+argument and returns a new array "mirrored" as shown in the examples:
+
+Examples:
+
+mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+***********************************************************************/
+
+const mirrorArray = (array) => {
+  // Your code here
+};
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = mirrorArray;
+} catch (e) {
+  return null;
+}
---
+++ appacademy/front-end/aa-projects/practice-for-week-02-scope-problems-without-context-main/problems/17-arrow-reverse.js
@@ -0,0 +1,22 @@
+/***********************************************************************
+Write a function reverseStr(str) that accepts a string and returns that string
+reversed. 
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = reverseStr("hello"); // returns "olleh"
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
+
+
+***********************************************************************/
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+    module.exports = reverseStr;
+  } catch (e) {
+    return null;
+  }
