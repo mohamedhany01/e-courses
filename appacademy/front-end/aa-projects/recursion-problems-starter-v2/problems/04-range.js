83f84e13757720ca2e4fdc91b1303c5cd488b25c
mohamedhany01 <mohamedhyouns@gmail.com>
2024-02-27 18:08:57 +0200

    Appacademy | Front-End | solving-problems-recursively-project
    - Starter files

---
+++ appacademy/front-end/aa-projects/recursion-problems-starter-v2/problems/04-range.js
@@ -0,0 +1,22 @@
+/***********************************************************************
+Write a recursive function, `range`, that takes a start and an end and returns
+an array of all numbers in that range, exclusive. If the end number is less than
+the start, return an empty array.
+
+Examples:
+
+range(1, 5); // [1, 2, 3, 4]
+range(3, 4); // [3]
+range(7, 6); // []
+***********************************************************************/
+
+
+// your code here
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = range;
+} catch (e) {
+  module.exports = null;
+}
---
+++ appacademy/front-end/aa-projects/recursion-problems-starter-v2/problems/11-flatten.js
@@ -0,0 +1,20 @@
+/***********************************************************************
+Write a recursive function called `flatten` that takes a single array with
+any number of nested arrays and returns and array with all the nested
+contents on one level.
+
+Examples:
+
+flatten([]); // []
+flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
+***********************************************************************/
+
+// your code here
+  
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = flatten;
+} catch (e) {
+  module.exports = null;
+}
