90407b0dec494cba7981282c6f9cf9c6178102eb
mohamedhany01 <mohamedhyouns@gmail.com>
2024-03-14 13:56:16 +0200

    Appacademy | Front-End | practice-for-week-04-inheritance
        - Finish challenge

--- appacademy/front-end/aa-projects/practice-for-week-04-inheritance-main/classes/student.js
+++ appacademy/front-end/aa-projects/practice-for-week-04-inheritance-main/classes/student.js
@@ -1,6 +1,23 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(s1, s2) {
+    if (s1.GPA > s2.GPA) {
+      return `${s1.firstName} ${s1.lastName} has the higher GPA.`;
+    } else if (s2.GPA > s1.GPA) {
+      return `${s2.firstName} ${s2.lastName} has the higher GPA.`;
+    } else {
+      return "Both students have the same GPA"
+    }
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
