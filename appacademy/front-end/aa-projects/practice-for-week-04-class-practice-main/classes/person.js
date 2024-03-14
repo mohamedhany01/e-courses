d0984529c6d7d36e328eac2f5ec0d8dfb5eb6496
mohamedhany01 <mohamedhyouns@gmail.com>
2024-03-14 13:25:36 +0200

    Appacademy | Front-End | practice-for-week-04-class-practice
    - Finish challenge

--- appacademy/front-end/aa-projects/practice-for-week-04-class-practice-main/classes/person.js
+++ appacademy/front-end/aa-projects/practice-for-week-04-class-practice-main/classes/person.js
@@ -1,4 +1,34 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old`);
+  }
+
+  static introducePeople(persons) {
+    if (!Array.isArray(persons)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+      const element = persons[i];
+      if (!(element instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.");
+      }
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+      const element = persons[i];
+      element.introduce();
+    }
+
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
