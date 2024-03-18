f6b75f8e32fb0957df753ab978293b613967e5e1
mohamedhany01 <mohamedhyouns@gmail.com>
2024-03-18 17:48:56 +0200

    Appacademy | Front-End | adventure-combat-starter
        - Starter files

---
+++ appacademy/front-end/aa-projects/adventure-combat-starter-master/class/character.js
@@ -0,0 +1,20 @@
+class Character {
+
+  constructor(name, description, currentRoom) {
+    // Fill this in
+
+  }
+
+  applyDamage(amount) {
+    // Fill this in
+  }
+
+  die() {
+    // Fill this in
+  }
+
+}
+
+module.exports = {
+  Character,
+};
---
+++ appacademy/front-end/aa-projects/adventure-combat-starter-master/class/player.js
@@ -0,0 +1,75 @@
+const {Character} = require('./character');
+const {Enemy} = require('./enemy');
+const {Food} = require('./food');
+
+class Player extends Character {
+
+  constructor(name, startingRoom) {
+    super(name, "main character", startingRoom);
+  }
+
+  move(direction) {
+
+    const nextRoom = this.currentRoom.getRoomInDirection(direction);
+
+    // If the next room is valid, set the player to be in that room
+    if (nextRoom) {
+      this.currentRoom = nextRoom;
+
+      nextRoom.printRoom(this);
+    } else {
+      console.log("You cannot move in that direction");
+    }
+  }
+
+  printInventory() {
+    if (this.items.length === 0) {
+      console.log(`${this.name} is not carrying anything.`);
+    } else {
+      console.log(`${this.name} is carrying:`);
+      for (let i = 0 ; i < this.items.length ; i++) {
+        console.log(`  ${this.items[i].name}`);
+      }
+    }
+  }
+
+  takeItem(itemName) {
+
+    // Fill this in
+
+  }
+
+  dropItem(itemName) {
+
+    // Fill this in
+
+  }
+
+  eatItem(itemName) {
+
+    // Fill this in
+
+  }
+
+  getItemByName(name) {
+
+    // Fill this in
+
+  }
+
+  hit(name) {
+
+    // Fill this in
+
+  }
+
+  die() {
+    console.log("You are dead!");
+    process.exit();
+  }
+
+}
+
+module.exports = {
+  Player,
+};
