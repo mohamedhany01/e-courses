b500ded2f11f548d1449ec769c0510a9d2dc376e
mohamedhany01 <mohamedhyouns@gmail.com>
2024-03-24 17:43:33 +0200

    Appacademy | Front-End | practice-for-week-04-bind
     - Finish challenge

--- appacademy/front-end/aa-projects/practice-for-week-04-bind-main/test.js
+++ appacademy/front-end/aa-projects/practice-for-week-04-bind-main/test.js
@@ -1,1 +1,11 @@
-// Your code here
+const Employee = require('./employee');
+
+const johnWick = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(function() {
+    johnWick.sayName();
+}, 2000);
+
+setTimeout(function() {
+    johnWick.sayOccupation()
+}, 3000);
