60d3fbbe760e169514c178e37cf8ec06efbe030c
mohamedhany01 <mohamedhyouns@gmail.com>
2024-04-24 18:05:40 +0200

    Appacademy | Front-End | practice-for-week-04-custom-error-types
    - Finish files

--- appacademy/front-end/aa-projects/practice-for-week-04-custom-error-types-main/error-types/maximum-length-exceeded-error.js
+++ appacademy/front-end/aa-projects/practice-for-week-04-custom-error-types-main/error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,21 @@
 const ValidationError = require('./validation-error');
 
-// Your code here
+class MaximumLengthExceededError extends ValidationError {
+
+  constructor(excessLength, message = "Maximum length exceeded", ...args) {
+    super(...args);
+
+    this.message = message;
+
+    if (excessLength) {
+      this.excessLength = excessLength;
+      this.message = `Maximum length exceeded by ${this.excessLength}`;
+    }
+
+    this.name = 'MaximumLengthExceededError';
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- appacademy/front-end/aa-projects/practice-for-week-04-custom-error-types-main/error-types/validation-error.js
+++ appacademy/front-end/aa-projects/practice-for-week-04-custom-error-types-main/error-types/validation-error.js
@@ -1,4 +1,21 @@
-// Your code here
+class ValidationError extends Error {
+  constructor(message = "Invalid input", ...args) {
+
+    // Pass remaining arguments to parent constructor
+    super(...args);
+
+    this.message = message;
+
+    // Maintains proper stack trace for where error was thrown (available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    // The name property should match the class's name
+    this.name = 'ValidationError';
+
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
