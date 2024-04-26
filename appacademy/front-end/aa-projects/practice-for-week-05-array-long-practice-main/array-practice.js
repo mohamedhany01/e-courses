c2d8d750214de88894a295ff598c4745a37667ec
mohamedhany01 <mohamedhyouns@gmail.com>
2024-04-26 16:37:38 +0300

    Appacademy | Front-End | practice-for-week-05-array-long
    - Finish files

--- appacademy/front-end/aa-projects/practice-for-week-05-array-long-practice-main/array-practice.js
+++ appacademy/front-end/aa-projects/practice-for-week-05-array-long-practice-main/array-practice.js
@@ -1,38 +1,141 @@
+// T => O(N)
+// S => O(1)
 const findMinimum = arr => {
 
-  // Your code here
+  if (arr.length === 0) {
+    return undefined;
+  }
+
+  let min = Infinity;
+
+  for (const n of arr) {
+    if (n < min) {
+      min = n;
+    }
+  }
+
+  return min;
 
 };
 
+// T => O(N)
+// S => O(N)
 const runningSum = arr => {
+  if (arr.length === 0) {
+    return [];
+  }
 
-  // Your code here
+  const result = [];
+  let prev = 0;
+
+  for (const n of arr) {
+    prev = n + prev;
+
+    result.push(prev);
+  }
+
+  return result;
 };
 
+// T => O(N)
+// S => O(1)
 const evenNumOfChars = arr => {
+  let result = 0;
 
-  // Your code here
+  for (const str of arr) {
+
+    if (str.length % 2 === 0) {
+      result++;
+    }
+  }
+
+  return result;
 };
 
+// T => O(N^2)
+// S => O(N)
 const smallerThanCurr = arr => {
 
-  // Your code here
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+
+    let counter = 0;
+    for (let j = 0; j < arr.length; j++) {
+      const next = arr[j];
+
+      if (next < current) {
+        counter++;
+      }
+
+    }
+
+    result.push(counter);
+  }
+
+  return result;
 
 };
 
+// T => O(N^2)
+// S => O(1)
 const twoSum = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
 
-  // Your code here
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      const next = arr[j];
+
+      if (current + next === target) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
 
+// T => O(N)
+// S => O(1)
 const secondLargest = arr => {
+  if (arr.length < 2) {
+    return undefined;
+  }
 
-  // Your code here
+  if (arr.length === 2) {
+    return Math.min(arr[0], arr[1]);
+  }
+
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (const n of arr) {
+    if (n > max) {
+      second = max;
+      max = n;
+    } else if (n > second && n !== max) {
+      second = n;
+    }
+  }
+
+  return second;
 };
 
+// T => O(N)
+// S => O(N)
 const shuffle = (arr) => {
+  const result = [...arr];
 
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Fisher-Yates algorithm
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+
+  return result;
 };
 
 
