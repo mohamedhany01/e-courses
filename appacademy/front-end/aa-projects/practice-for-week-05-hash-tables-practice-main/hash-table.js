0d55a46c3daa0826a7ace12d441fd657596d33e6
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-06 15:43:57 +0300

    Appacademy | Front-End | practice-for-week-05-hash-tables
    - Finish files

--- appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-practice-main/hash-table.js
+++ appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-practice-main/hash-table.js
@@ -11,27 +11,88 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.capacity = numBuckets;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    const hash = sha256(key).slice(0, 8);
+    const hashDecimal = parseInt(hash, 16);
+    return hashDecimal;
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    const keyIndex = this.hashMod(key);
+
+    const isExist = this.data[keyIndex];
+
+    if (isExist) {
+      throw new Error("hash collision or same key/value pair already exists!");
+    }
+
+    this.data[keyIndex] = { key: key, value: value };
+
+    this.count++;
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    try {
+      this.insertNoCollisions(key, value);
+    } catch (error) {
+      const newPairs = new KeyValuePair(key, value);
+      const currentHead = this.data[0];
+
+      this.data[0] = newPairs;
+      newPairs.next = currentHead;
+
+      this.count++;
+    }
+  }
+
+  _getKey(key) {
+    // const isExist = this.data[keyIndex];
+
+    for (let i = 0; i < this.data.length; i++) {
+      const bucket = this.data[i];
+
+      if (bucket?.next) {
+        let head = bucket;
+
+        while (head) {
+          if (head.key === key) {
+            return head;
+          }
+
+          head = head.next;
+        }
+
+        continue;
+      }
+
+      if (bucket?.key === key) {
+        return bucket;
+      }
+
+      return false;
+    }
+
   }
 
   insert(key, value) {
-    // Your code here
+
+    const bucket = this._getKey(key);
+    const isSameKey = bucket?.key === key;
+
+    if (bucket && isSameKey) {
+      bucket.value = value;
+    } else {
+      this.insertWithHashCollisions(key, value);
+    }
   }
 
 }
