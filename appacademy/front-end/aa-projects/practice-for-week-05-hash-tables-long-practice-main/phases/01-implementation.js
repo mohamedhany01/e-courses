b4d61f6e7874ae552b802ad70fdb79326f443a39
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-07 17:25:06 +0300

    Appacademy | Front-End | practice-for-week-05-hash-tables-long
        - Finish files
        - Phase three not finished

--- appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-long-practice-main/phases/01-implementation.js
+++ appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-long-practice-main/phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.capacity = numBuckets;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,22 +31,137 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+
+    if (this.count / this.capacity >= 0.75) {
+      // debugger;
+      this.resize();
+    }
+
+    // Insert with collision
+    const bucket = this.hasCollision(key);
+    const keyIndex = this.hashMod(key);
+
+    if (!bucket) {
+      this.data[keyIndex] = { key: key, value: value };
+      this.count++;
+      return;
+    }
+
+    if (bucket) {
+      if (bucket.next) {
+        let currentBucket = bucket;
+
+        while (currentBucket) {
+          if (currentBucket.key === key) {
+            currentBucket.value = value;
+
+            return;
+          }
+
+          currentBucket = currentBucket.next;
+        }
+      }
+
+      const newBucket = new KeyValuePair(key, value);
+      newBucket.next = bucket;
+      this.data[keyIndex] = newBucket;
+      this.count++;
+    }
+  }
+
+  hasCollision(key) {
+    const keyIndex = this.hashMod(key);
+
+    return this.data[keyIndex];
   }
 
 
   read(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    const bucket = this.data[index];
+
+    if (bucket?.next) {
+      let next = bucket;
+
+      while (next) {
+        if (next && next.key === key) {
+          return next.value;
+        }
+        next = next.next;
+      }
+    }
+
+    if (bucket && bucket.key === key) {
+      return bucket.value;
+    }
+
+    return undefined;
   }
 
 
   resize() {
-    // Your code here
+    this.capacity = 2 * this.capacity;
+    const oldList = this.data;
+
+    this.data = new Array(this.capacity).fill(null);
+
+    this.count = 0;
+    for (let i = 0; i < oldList.length; i++) {
+      if (oldList[i]?.next) {
+
+        let next = oldList[i];
+
+        while (next) {
+          const { key, value } = next;
+          this.insert(key, value);
+
+          next = next.next;
+        }
+
+      } else {
+        const bucket = oldList[i];
+
+        if (!bucket) continue;
+
+        const { key, value } = bucket;
+        this.insert(key, value);
+      }
+    }
   }
 
 
   delete(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let bucket = this.data[index];
+
+    if (bucket && !bucket.next && key === bucket.key) {
+      this.data[index] = null;
+      this.count--;
+      return;
+    }
+
+    if (bucket?.next) {
+      let next = bucket;
+      let prev = bucket;
+
+      if (next && next.key === key) {
+        this.data[index] = next.next;
+        this.count--;
+        return;
+      }
+
+      while (next) {
+        if (next && next.key === key) {
+          prev.next = prev.next.next;
+          this.count--;
+          return;
+        }
+        prev = next;
+        next = next.next;
+      }
+    }
+
+    return "Key not found";
   }
 }
 
--- appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
+++ appacademy/front-end/aa-projects/practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
@@ -1,26 +1,94 @@
 function anagrams(str1, str2) {
-  // Your code here
+  if (str1.length !== str2.length) return false;
+
+  const map = new Map();
+  const len = str1.length;
+
+  for (let i = 0; i < len; i++) {
+    const charKey = str1[i];
+    map.set(charKey, charKey);
+  }
+
+  for (let i = 0; i < len; i++) {
+    if (!map.get(str2[i])) return false;
+  }
+
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  let max = arr1;
+  let min = arr2;
+
+  if (arr2.length > max.length) {
+    max = arr2;
+    min = arr1;
+  }
+
+  const s1 = new Set(max);
+  const s2 = new Set(min);
+
+  const common = [...s1].filter(e => s2.has(e));
+
+  return common;
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map.get(arr[i])) {
+      return arr[i];
+    }
+    map.set(arr[i], arr[i]);
+  }
+
+  return null;
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.get(complement)) {
+      return true;
+    }
+    map.set(nums[i], i);
+  }
+
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
-  // Your code here
+  if (pattern.length !== strings.length) {
+    return false;
+  }
+
+  const patternMap = new Map();
+  const stringMap = new Map();
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = strings[i];
+
+    if (!patternMap.get(char) && !stringMap.get(word)) {
+      patternMap.set(char, word);
+      stringMap.set(word, char);
+    } else {
+      if (patternMap.get(char) !== word || stringMap.get(word) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
+
 
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
