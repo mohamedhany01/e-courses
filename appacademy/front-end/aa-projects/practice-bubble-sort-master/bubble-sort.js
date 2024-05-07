daeba61d3dd484deb427602253127c748befd87a
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-07 18:37:03 +0300

    Appacademy | Front-End | practice-bubble-sort
    - Finish files

--- appacademy/front-end/aa-projects/practice-bubble-sort-master/bubble-sort.js
+++ appacademy/front-end/aa-projects/practice-bubble-sort-master/bubble-sort.js
@@ -1,17 +1,26 @@
 
+// Iterate through the array
+
+// If the current value is greater than its neighbor to the right
+// Swap those values
+
+// If you get to the end of the array and no swaps have occurred, return
+
+// Otherwise, repeat from the beginning
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      const currentElement = arr[j];
+      const newElement = arr[j + 1];
 
-    // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
+      if (currentElement > newElement) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
         console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
+      }
+    }
+  }
 
 }
 
