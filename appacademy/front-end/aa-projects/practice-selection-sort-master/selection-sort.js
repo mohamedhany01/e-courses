875c63ee708f6a9633838432b356f37ce02dd537
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-07 21:09:25 +0300

    Appacademy | Front-End | practice-selection-sort
    - Finish files

--- appacademy/front-end/aa-projects/practice-selection-sort-master/selection-sort.js
+++ appacademy/front-end/aa-projects/practice-selection-sort-master/selection-sort.js
@@ -8,15 +8,28 @@ function selectionSort(arr) {
 
   // While the array is not empty...
 
-    // Do not move this console.log
+  // Find the index of the minimum value in the unsorted half
+
+  // Save and remove the value at the min index
+
+  // Add the min value to the end of the sorted array
+
+  const tempArray = [...arr];
+  const sorted = [];
+  console.log("");
+  while (tempArray.length - 1 >= 0) {
+    let minIndex = 0;
+    for (let i = 1; i < tempArray.length; i++) {
+      if (tempArray[i] < tempArray[minIndex]) {
+        minIndex = i;
+      }
+    }
+
+    sorted.push(tempArray[minIndex]);
+    tempArray.splice(minIndex, 1);
     console.log(sorted.join(","));
-
-    // Find the index of the minimum value in the unsorted half
-
-    // Save and remove the value at the min index
-
-    // Add the min value to the end of the sorted array
-
+  }
+  return sorted;
 }
 
 
@@ -27,18 +40,41 @@ function selectionSortInPlace(arr) {
 
   // Repeat while the unsorted half is not empty:
 
-    // Do not move this console.log
+  // Find the index of the minimum value in the unsorted half
+
+  // Save the min value
+
+  // Shift every unsorted value to the left of the min value to the right by 1
+
+  // Put the min value at the divider
+
+  // Increment the divider and repeat
+
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
     console.log(arr.join(","));
 
-    // Find the index of the minimum value in the unsorted half
+    let minIndex = i;
 
-    // Save the min value
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
 
-    // Shift every unsorted value to the left of the min value to the right by 1
+    const minValue = arr[minIndex];
 
-    // Put the min value at the divider
+    // Shift all elements between minIndex and i forward by one place
+    for (let k = minIndex; k > i; k--) {
+      arr[k] = arr[k - 1];
+    }
 
-    // Increment the divider and repeat
+    // Place the minimum value at the correct position
+    arr[i] = minValue;
+  }
+
+  console.log(arr.join(","));
 
 }
 
