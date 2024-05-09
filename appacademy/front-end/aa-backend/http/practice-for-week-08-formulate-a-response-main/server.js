428798b81280781b6b585188a2a63110e61734c0
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-09 19:54:22 +0300

    Appacademy | Back-End | practice-for-week-08-formulate-a-response-main
        - Finish files

--- appacademy/front-end/aa-backend/http/practice-for-week-08-formulate-a-response-main/server.js
+++ appacademy/front-end/aa-backend/http/practice-for-week-08-formulate-a-response-main/server.js
@@ -1,1 +1,23 @@
-// Your code here
+const app = require('http');
+const PORT = 5666;
+
+app.createServer((req, res) => {
+    const responseBody = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello there!</h1>
+        </body>
+        </html>
+`;
+
+    res.setHeader("Content-Type", "text/html");
+
+    res.end(responseBody);
+
+}).listen(PORT);
