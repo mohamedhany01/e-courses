06fc76a09aa02ea23a0da93a64000595b564db3f
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-12 18:37:24 +0300

    Appacademy | Back-End | practice-for-week-08-route-handlers
        - Finish files

--- appacademy/front-end/aa-backend/http/practice-for-week-08-route-handlers-main/server.js
+++ appacademy/front-end/aa-backend/http/practice-for-week-08-route-handlers-main/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 let nextDogId = 1;
 
@@ -6,6 +7,103 @@ function getNewDogId() {
   const newDogId = nextDogId;
   nextDogId++;
   return newDogId;
+}
+
+const dogsDB = new Map();
+
+function processRoute(url, method, res, req) {
+  let data = "";
+
+  const paths = url.split('/');
+  const isDogID = typeof (parseInt(paths[2], 10)) === 'number';
+  const isNewDog = paths[2] === "new" && typeof (paths[2]) === 'string';
+
+
+  if (method === 'GET') {
+
+    if (url === "/") {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      data = 'Dog Club';
+      return data;
+    }
+
+    if (paths.length === 2 && paths[1] === "dogs") {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      data = 'Dogs index';
+      return data;
+    }
+
+    if (paths.length === 3 && isDogID) {
+      const id = parseInt(paths[2], 10);
+      if (dogsDB.has(id)) {
+        data = dogsDB.get(id);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        return data.name;
+      }
+    }
+
+    if (paths.length === 3 && isNewDog) {
+      const addForm = fs.readFileSync('./index.html', 'utf-8');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      return addForm;
+    }
+
+    if (paths.length === 4 && isDogID && paths[3] === "edit") {
+      const id = parseInt(paths[2], 10);
+      if (dogsDB.has(id)) {
+        data = dogsDB.get(id);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        data = `Dog edit form page for dogId ${id}`;
+
+        return data;
+      }
+    }
+  }
+
+  if (method === 'POST') {
+    if (paths.length === 3 && isDogID) {
+      const id = parseInt(paths[2], 10);
+      if (dogsDB.has(id)) {
+        data = dogsDB.get(id);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Location', `/dogs/${newID}`);
+
+        data = "NONE";
+        return data;
+      }
+    }
+
+    if (paths.length === 2 && paths[1] === "dogs") {
+      const newID = getNewDogId();
+      const dogName = req.body.name;
+
+      dogsDB.set(newID, {
+        id: newID,
+        name: dogName
+      });
+
+      res.statusCode = 302;
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Location', `/dogs/${newID}`);
+
+      data = "NONE";
+      return data;
+    }
+  }
+
+  return data;
 }
 
 const server = http.createServer((req, res) => {
@@ -33,7 +131,13 @@ const server = http.createServer((req, res) => {
     }
     // Do not edit above this line
 
-    // define route handlers here
+    const { url, method } = req;
+
+    const result = processRoute(url, method, res, req);
+
+    if (result) {
+      return res.end(result);
+    }
 
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
