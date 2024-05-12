ac9e44317bb6f184cafdc271830f268052c293f6
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-12 20:01:07 +0300

    Appacademy | Back-End | practice-for-week-08-serve-static-assets
    - Finish files

--- appacademy/front-end/aa-backend/http/practice-for-week-08-serve-static-assets-main/server.js
+++ appacademy/front-end/aa-backend/http/practice-for-week-08-serve-static-assets-main/server.js
@@ -1,8 +1,37 @@
 const http = require('http');
 const fs = require("fs");
 
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 const server = http.createServer((req, res) => {
-  // Your code here
+
+  const index = fs.readFileSync("./index.html", 'utf8');
+  const { url } = req;
+
+  // Parsing url when having static files
+  if (url !== "/") {
+    const paths = url.split('/');
+    const [, fileType] = paths[paths.length - 1].split('.');
+    const fileName = paths[paths.length - 1];
+
+    switch (fileType) {
+      case 'jpg':
+        const imgPath = `./assets/images/${fileName}`;
+        const img = fs.readFileSync(imgPath);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/jpeg');
+        return res.end(img);
+      case 'css':
+        const stylePath = `./assets/css/${fileName}`;
+        const style = fs.readFileSync(stylePath, 'utf8');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        return res.end(style);
+    }
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  return res.end(index);
 });
 
 const port = 5000;
