fd1cfbb72b00b0daad84e2760e9a69b3d621edd6
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-13 19:05:27 +0300

    Appacademy | Back-End | practice-for-week-08-promise-all
    - Finish files

--- appacademy/front-end/aa-backend/promises/practice-for-week-08-promise-all-main/phase-1.js
+++ appacademy/front-end/aa-backend/promises/practice-for-week-08-promise-all-main/phase-1.js
@@ -26,13 +26,11 @@ function liftWeights() {
 }
 
 function workout() {
-  // refactor this code to use Promise.all
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
-    .catch((err) => console.log(err));
+  Promise.all([stretch(), runOnTreadmill(), liftWeights()]).then(() => {
+    console.log("done working out");
+  }).catch((err) => console.log(err));
 }
+
 
 
 /* ============================ TEST YOUR CODE ============================
@@ -43,8 +41,8 @@ output.
 
 
 workout();
-  // should print out the following:
-    // done running on treadmill
-    // done stretching
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done running on treadmill
+// done stretching
+// done lifting weights
+// done working out
