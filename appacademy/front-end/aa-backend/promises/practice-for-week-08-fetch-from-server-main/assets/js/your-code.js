543671755861399b3ed090926e96d16b15d5f342
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-15 20:28:18 +0300

    Appacademy | Back-End | practice-for-week-08-fetch-from-server
                    - Finish files

--- appacademy/front-end/aa-backend/promises/practice-for-week-08-fetch-from-server-main/assets/js/your-code.js
+++ appacademy/front-end/aa-backend/promises/practice-for-week-08-fetch-from-server-main/assets/js/your-code.js
@@ -1,19 +1,43 @@
 export function getAllDogs() {
-    // Your code here
+    return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+    return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+
+    return fetch("/dogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            name: "Foo",
+            age: 100
+        })
+    });
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+    return fetch("/dogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            name: name,
+            age: age
+        })
+    });
 }
 
 export function deleteDog(id) {
-      // Your code here
+    return fetch(`/dogs/${id}/delete`, {
+        method: "POST",
+        headers: {
+            "AUTH": "ckyut5wau0000jyv5bsrud90y"
+        }
+    });
 }
