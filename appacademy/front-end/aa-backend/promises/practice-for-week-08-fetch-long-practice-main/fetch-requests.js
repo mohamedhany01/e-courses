b7a29a500b327af270edbcd88ceefaac88ee8a5f
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-23 17:19:47 +0300

    Appacademy | Back-End | practice-for-week-08-fetch-long-practice
    - Finish files

--- appacademy/front-end/aa-backend/promises/practice-for-week-08-fetch-long-practice-main/fetch-requests.js
+++ appacademy/front-end/aa-backend/promises/practice-for-week-08-fetch-long-practice-main/fetch-requests.js
@@ -5,16 +5,39 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
+fetch("http://localhost:5000/products/", {
+    method: "POST",
+    body: "name=Buz&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+}).then(response => response).then(data => console.log(data));
 
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
+fetch("http://localhost:5000/products/", {
+    method: "POST",
+    body: "name=Buz&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+}).then(response => response);
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here
+fetch("http://localhost:5000/products/", {
+    method: "POST",
+    body: new URLSearchParams({
+        name: "Caribbean Delight Coffee",
+        description: "Made by Manatee Coffee",
+        price: 11.99,
+        categories: "grocery"
+    }),
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+}).then(response => response).then(data => console.log(data));
