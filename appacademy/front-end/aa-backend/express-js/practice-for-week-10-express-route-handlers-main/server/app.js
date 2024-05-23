c74ba851b8ed90d00bec773ab26e7dc49cbbcaf8
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-23 15:57:27 +0300

    Appacademy | Back-End | practice-for-week-10-express-route-handlers
    - Finish files

--- appacademy/front-end/aa-backend/express-js/practice-for-week-10-express-route-handlers-main/server/app.js
+++ appacademy/front-end/aa-backend/express-js/practice-for-week-10-express-route-handlers-main/server/app.js
@@ -24,7 +24,265 @@ const {
 const express = require('express');
 const app = express();
 
-// Your code here
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get("/artists", (req, res) => {
+
+  const results = getAllArtists();
+
+  return res.json(results);
+});
+
+app.post("/artists", (req, res) => {
+
+  const { name } = req.body;
+
+  const artist = {
+    name: name
+  }
+
+  const results = addArtist(artist);
+
+  res.status(201);
+
+  return res.json(results);
+});
+
+app.get("/artists/latest", (req, res) => {
+
+  const results = getLatestArtist();
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.get("/artists/latest/albums", (req, res) => {
+
+  const results = getAlbumsForLatestArtist();
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.get("/artists/:artistId", (req, res) => {
+
+  const { artistId } = req.params;
+
+  const results = getArtistByArtistId(artistId)
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.put("/artists/:artistId", (req, res) => {
+
+  const { artistId } = req.params;
+  const { name } = req.body;
+
+  const results = editArtistByArtistId(artistId, { name: name })
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.patch("/artists/:artistId", (req, res) => {
+
+  const { artistId } = req.params;
+  const { name } = req.body;
+
+  const results = editArtistByArtistId(artistId, { name: name })
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.delete("/artists/:artistId", (req, res) => {
+
+  const { artistId } = req.params;
+
+  const results = deleteArtistByArtistId(parseInt(artistId, 10))
+
+  res.status(200);
+
+  return res.json({
+    "message": "Successfully deleted"
+  });
+});
+
+app.get("/artists/:artistId/albums", (req, res) => {
+
+  const { artistId } = req.params;
+
+  const results = getAlbumsByArtistId(artistId)
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.get("/albums/:albumId", (req, res) => {
+
+  const { albumId } = req.params;
+
+  const results = getAlbumByAlbumId(albumId)
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.post("/artists/:artistId/albums", (req, res) => {
+
+  const { name } = req.body;
+  const { artistId } = req.params;
+
+  const results = addAlbumByArtistId(artistId, { name: name })
+
+  res.status(201);
+
+  return res.json(results);
+});
+
+
+app.patch("/albums/:albumId", (req, res) => {
+
+  const { albumId } = req.params;
+  const { name } = req.body;
+
+  const results = editAlbumByAlbumId(albumId, { name: name })
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.put("/albums/:albumId", (req, res) => {
+
+  const { albumId } = req.params;
+  const { name } = req.body;
+
+  const results = editAlbumByAlbumId(albumId, { name: name })
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.delete("/albums/:albumId", (req, res) => {
+  const { albumId } = req.params;
+
+  deleteAlbumByAlbumId(albumId);
+
+  res.status(200);
+
+  return res.json({
+    "message": "Successfully deleted"
+  });
+
+});
+
+app.get("/albums", (req, res) => {
+
+  const { startsWith } = req.query;
+
+  const results = getFilteredAlbums(startsWith);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.get("/songs/:songId", (req, res) => {
+
+  const { songId } = req.params;
+
+  const results = getSongBySongId(songId);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.post("/albums/:albumId/songs", (req, res) => {
+
+  const { albumId } = req.params;
+  const data = req.body;
+
+  const results = addSongByAlbumId(albumId, data);
+
+  res.status(201);
+
+  return res.json(results);
+});
+
+app.get("/artists/:artistId/songs", (req, res) => {
+
+  const { artistId } = req.params;
+
+  const results = getSongsByArtistId(artistId);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.get("/albums/:albumId/songs", (req, res) => {
+
+  const { albumId } = req.params;
+
+  const results = getSongsByAlbumId(albumId);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.put("/songs/:songId", (req, res) => {
+
+  const { songId } = req.params;
+  const data = req.body;
+
+  const results = editSongBySongId(songId, data);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.patch("/songs/:songId", (req, res) => {
+
+  const { songId } = req.params;
+  const data = req.body;
+
+  const results = editSongBySongId(songId, data);
+
+  res.status(200);
+
+  return res.json(results);
+});
+
+app.delete("/songs/:songId", (req, res) => {
+  const { songId } = req.params;
+
+  deleteSongBySongId(songId);
+
+  res.status(200);
+
+  return res.json({
+    "message": "Successfully deleted"
+  });
+
+});
+
 
 // DO NOT MODIFY
 if (require.main === module) {
