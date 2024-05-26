8aae4eda8415b10eedbf53c5303f524f0d2d4fa1
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-26 21:06:54 +0300

    Appacademy | Back-End | practice-for-week-10-express-error-handling-404
    - Finish files

--- appacademy/front-end/aa-backend/express-js/practice-for-week-10-express-error-handling-404-main/server/app.js
+++ appacademy/front-end/aa-backend/express-js/practice-for-week-10-express-error-handling-404-main/server/app.js
@@ -5,5 +5,25 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.get("/some-generic-error", (req, res) => {
+  throw new Error("Generic error");
+});
+
+// "Resource Not Found" Middleware
+app.use((req, res, next) => {
+  const error = new Error("Sorry, the requested resource couldn't be found.");
+  error.statusCode = 404;
+  next(error);
+});
+
+// Catch-all Error Handling Middleware
+app.use((err, req, res, next) => {
+  // console.error(err.stack);
+  res.status(err.statusCode || 500).json({
+    message: err.message,
+    statusCode: err.statusCode || 500,
+  });
+});
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
