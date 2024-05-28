0fb41aaa9903062e013841ee9ad333cd5cf58f32
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-28 16:43:12 +0300

    Appacademy | Back-End | catstagram-day-2
        - Starter files

---
+++ appacademy/front-end/aa-backend/webstorage/practice-for-week-09-catsagram-long-practice/practice-for-week-09-catsagram-long-practice-day-2-main/public/main.js
@@ -0,0 +1,30 @@
+export const createMainContent = () => {
+    // Create h1
+    const h1 = document.createElement("h1");
+    h1.innerText = "Catstagram";
+
+    // Create img
+    const img = document.createElement("img");
+    img.style.margin = "20px";
+    img.style.maxWidth = "750px";
+
+    const container = document.querySelector(".container");
+    container.appendChild(h1);
+    container.appendChild(img);
+
+    fetchImage();
+};
+
+const fetchImage = async () => {
+    // Fetch image from API and set img url
+    try {
+        const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
+        // Converts to JSON
+        const kittenData = await kittenResponse.json();
+        // console.log(kittenData);
+        const kittenImg = document.querySelector("img");
+        kittenImg.src = kittenData[0].url;
+    } catch (e) {
+        console.log("Failed to fetch image", e);
+    }
+};
