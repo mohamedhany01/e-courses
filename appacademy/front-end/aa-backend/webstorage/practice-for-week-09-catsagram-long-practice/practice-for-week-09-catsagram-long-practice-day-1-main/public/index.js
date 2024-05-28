e694f78eae8367ee872f0675c2fd5d68be3f0db8
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-28 16:40:40 +0300

    Appacademy | Back-End | catstagram-day-1
    - Finish files

--- appacademy/front-end/aa-backend/webstorage/practice-for-week-09-catsagram-long-practice/practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
+++ appacademy/front-end/aa-backend/webstorage/practice-for-week-09-catsagram-long-practice/practice-for-week-09-catsagram-long-practice-day-1-main/public/index.js
@@ -1,1 +1,14 @@
-// Your code here
+window.addEventListener("load", () => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then((data) => data.json())
+        .then(([{ id, url, width, height }]) => {
+            const html = `
+                <div class="cat-img">
+                    <h1>Kitten Pic</h1>
+                    <img src=${url} alt=${id} width=${width} height=${height}>
+                </div>
+            `;
+
+            document.body.insertAdjacentHTML("afterbegin", html);
+        });
+});
