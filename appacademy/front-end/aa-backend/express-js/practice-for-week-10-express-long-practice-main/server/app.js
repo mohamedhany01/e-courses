63f9ffc48478af34fbbe0fca2e0d7ab9f970e784
mohamedhany01 <mohamedhyouns@gmail.com>
2024-05-30 16:50:44 +0300

    Appacademy | Back-End | practice-for-week-10-express-long-practice
        - Starter files

---
+++ appacademy/front-end/aa-backend/express-js/practice-for-week-10-express-long-practice-main/server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// For testing purposes, GET /
+app.get('/', (req, res) => {
+  res.json("Express server running. No content provided at root level. Please use another route.");
+});
+
+// For testing express.json middleware
+app.post('/test-json', (req, res, next) => {
+  // send the body as JSON with a Content-Type header of "application/json"
+  // finishes the response, res.end()
+  res.json(req.body);
+  next();
+});
+
+// For testing express-async-errors
+app.get('/test-error', async (req, res) => {
+  throw new Error("Hello World!")
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
