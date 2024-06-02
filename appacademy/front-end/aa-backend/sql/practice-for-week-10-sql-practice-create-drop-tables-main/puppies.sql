b95b36a8bb858351e4b1b5db2a95a98eaab57c8c
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-02 18:01:42 +0300

    Appacademy | Back-End | practice-for-week-10-sql-practice-create-drop-tables
    - Finish files

---
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-create-drop-tables-main/puppies.sql
@@ -0,0 +1,8 @@
+CREATE TABLE puppies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50),
+  age_yrs NUMERIC(3, 1),
+  breed VARCHAR(100),
+  weight_lbs INTEGER,
+  microchipped BOOLEAN
+)
