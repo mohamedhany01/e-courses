70c9300e188d1fad8b8c0efd9f2d7bfbf10f02af
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-02 16:05:05 +0300

    Appacademy | Back-End | practice-for-week-10-combining-express-sql-short
        - Starter files

---
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-combining-express-sql-short-practice-main/server/app.js
@@ -0,0 +1,74 @@
+// Instantiate Express and the application - DO NOT MODIFY
+const express = require('express');
+const app = express();
+
+// Database file - DO NOT MODIFY
+// DO NOT DO THIS - USE .env VARIABLE INSTEAD
+const DATA_SOURCE = 'app.db';
+
+/**
+ * Step 1 - Connect to the database
+ */
+// Your code here
+
+// Express using json - DO NOT MODIFY
+app.use(express.json());
+
+// List of all colors in the database - DO NOT MODIFY
+app.get('/colors', (req, res, next) => {
+    const sql = 'SELECT * FROM colors';
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+        res.json(rows);
+    });
+});
+
+// One color by id
+app.get('/colors/:id', (req, res, next) => {
+    /**
+     * STEP 2A - SQL Statement
+     */
+    // Your code here
+
+    /**
+     * STEP 2B - SQL Parameters
+     */
+    // Your code here
+
+    /**
+     * STEP 2C - Call database function
+     *  - return response
+     */
+    // Your code here
+});
+
+// Add color
+app.get('/colors/add/:name', (req, res, next) => {
+    // SQL INSERT
+    const sql = "INSERT INTO colors (name) VALUES (?)";
+    const params = [req.params.name];
+
+    // SQL QUERY NEW ROW
+    const sqlLast = 'SELECT * FROM colors ORDER BY id DESC LIMIT 1';
+
+    /**
+     * STEP 3 - After INSERT, return the new row
+     *  - insert
+     *  - if error, go to next()
+     *  - if no error, query for new row
+     *  - return new row
+     */
+    // Your code here
+})
+
+// Root route - DO NOT MODIFY
+app.get('/', (req, res) => {
+    res.json({
+        message: "API server is running"
+    });
+});
+
+// Set port and listen for incoming requests - DO NOT MODIFY
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
