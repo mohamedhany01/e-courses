4f84767781da910de5fed592c6aaa5e31af195af
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-05 18:42:20 +0300

    Appacademy | Back-End | practice-for-week-10-sql-practice-create-related-tables
    - Finish files

--- appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
@@ -3,7 +3,7 @@ PRAGMA foreign_keys = 1;
 DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
-
+DROP TABLE IF EXISTS musicians_instruments;
 CREATE TABLE bands (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(100)
@@ -11,9 +11,17 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  band_id INTEGER NOT NULL,
+  FOREIGN KEY(band_id) REFERENCES bands(id)
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+CREATE TABLE musicians_instruments (
+  musician_id INTEGER NOT NULL,
+  instrument_id INTEGER NOT NULL,
+  FOREIGN KEY(musician_id) REFERENCES musicians(id),
+  FOREIGN KEY(instrument_id) REFERENCES instruments(id)
 );
