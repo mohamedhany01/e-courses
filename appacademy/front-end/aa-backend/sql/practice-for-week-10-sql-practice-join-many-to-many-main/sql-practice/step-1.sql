ba56746a28cb549f8fe5d845bcafaf5bd939d2a2
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-05 19:45:57 +0300

    Appacademy | Back-End | practice-for-week-10-sql-practice-join-many-to-many
    - Finish files

--- appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-1.sql
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-1.sql
@@ -2,4 +2,8 @@
 -- Connecting both sides of a many-to-many relationship involves JOINing the
 -- join table on to one side of the relationship, then JOINing the other side
 -- on to the join table.
--- Your code here
+SELECT musicians.first_name,
+    instruments.type
+FROM musicians
+    JOIN musician_instruments ON musician_instruments.musician_id = musicians.id
+    JOIN instruments ON musician_instruments.instrument_id = instruments.id;
--- appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-practice-join-many-to-many-main/sql-practice/step-2.sql
@@ -1,4 +1,10 @@
 -- Step 2
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
--- Your code here
+SELECT musicians.first_name,
+    musicians.last_name,
+    instruments.type
+FROM musicians
+    JOIN musician_instruments ON musician_instruments.musician_id = musicians.id
+    JOIN instruments ON musician_instruments.instrument_id = instruments.id
+WHERE instruments.type = "piano";
