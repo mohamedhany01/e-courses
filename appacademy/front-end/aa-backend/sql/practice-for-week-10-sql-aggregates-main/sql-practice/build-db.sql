a4db6ae77445172a4155f8123b7c5adb2812115d
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-09 16:39:12 +0300

    Appacademy | Back-End | practice-for-week-10-sql-aggregates
    - Finish files

--- appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-aggregates-main/sql-practice/build-db.sql
+++ appacademy/front-end/aa-backend/sql/practice-for-week-10-sql-aggregates-main/sql-practice/build-db.sql
@@ -1,27 +1,22 @@
-PRAGMA foreign_keys=on; -- Turns on foreign key support in SQLite3
-
+PRAGMA foreign_keys = on;
+-- Turns on foreign key support in SQLite3
 -- Create / re-create tables
 DROP TABLE IF EXISTS toys;
 DROP TABLE IF EXISTS cats;
-
 CREATE TABLE cats (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   birth_year INTEGER
 );
-
 CREATE TABLE toys (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   cat_id INTEGER,
   FOREIGN KEY (cat_id) REFERENCES cats(id) ON DELETE CASCADE
 );
-
 -- Seed Data
-INSERT INTO cats
-  (name, birth_year)
-VALUES
-  ('Smudge', 2014),
+INSERT INTO cats (name, birth_year)
+VALUES ('Smudge', 2014),
   ('Molly', 2015),
   ('Lucky', 2016),
   ('Bella', 2020),
@@ -29,11 +24,8 @@ VALUES
   ('Oscar', 2010),
   ('Garfield', 2009),
   ('Crookshanks', 2017);
-
-INSERT INTO toys
-  (cat_id, name)
-VALUES
-  (1, 'Catnip Mouse'),
+INSERT INTO toys (cat_id, name)
+VALUES (1, 'Catnip Mouse'),
   (2, 'Feather Wand'),
   (2, 'Scratcher'),
   (2, 'Laser Pointer'),
@@ -43,3 +35,25 @@ VALUES
   (5, 'Crinkle Ball'),
   (7, 'Cheetos'),
   (8, 'Yarn');
+-- Q1
+SELECT COUNT(*)
+FROM cats AS total_cats;
+-- Q2
+SELECT MAX(birth_year)
+FROM cats;
+SELECT MIN(birth_year)
+FROM cats;
+-- BONUS | Q1
+SELECT cats.name,
+  COUNT(*) AS total_toys_per_cat
+FROM cats
+  JOIN toys ON cats.id = toys.cat_id
+GROUP BY toys.cat_id
+ORDER BY total_toys_per_cat DESC;
+-- Q2
+SELECT cats.name,
+  COUNT(*) AS total_toys_per_cat
+FROM cats
+  JOIN toys ON cats.id = toys.cat_id
+GROUP BY toys.cat_id
+HAVING total_toys_per_cat >= 2;
