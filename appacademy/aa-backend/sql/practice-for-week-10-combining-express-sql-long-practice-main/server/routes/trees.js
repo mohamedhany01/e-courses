9a35bb316ebb044469c3ceb931c11b6b34e5efda
mohamedhany01 <mohamedhyouns@gmail.com>
2024-06-11 13:31:17 +0300

    Appacademy | Back-End | practice-for-week-10-combining-express-sql-long
    - Starter files

---
+++ appacademy/aa-backend/sql/practice-for-week-10-combining-express-sql-long-practice-main/server/routes/trees.js
@@ -0,0 +1,72 @@
+// Instantiate router - DO NOT MODIFY
+const express = require('express');
+const router = express.Router();
+
+/**
+ * BASIC PHASE 2, Step A - Instantiate SQLite and database
+ *   - Database file: "data_source" environment variable
+ *   - Database permissions: read/write records in tables
+ */
+// Your code here
+
+/**
+ * BASIC PHASE 2, Step B - List of all trees in the database
+ *
+ * Protocol: GET
+ * Path: /
+ * Parameters: None
+ * Response: JSON array of objects
+ *   - Object properties: height-ft, tree, id
+ *   - Ordered by the height_ft from tallest to shortest
+ */
+// Your code here
+
+/**
+ * BASIC PHASE 3 - Retrieve one tree with the matching id
+ *
+ * Path: /:id
+ * Protocol: GET
+ * Parameter: id
+ * Response: JSON Object
+ *   - Properties: id, tree, location, height_ft, ground_circumference_ft
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 4 - INSERT tree row into the database
+ *
+ * Path: /trees
+ * Protocol: POST
+ * Parameters: None
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
+ *
+ * Path: /trees/:id
+ * Protocol: DELETE
+ * Parameter: id
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
+ *
+ * Path: /trees/:id
+ * Protocol: PUT
+ * Parameter: id
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+// Export class - DO NOT MODIFY
+module.exports = router;
---
+++ appacademy/aa-backend/sql/practice-for-week-10-combining-express-sql-long-practice-main/server/seed-data.sql
@@ -0,0 +1,5 @@
+-- BASIC PHASE 1A - DROP and CREATE table
+-- Your code here
+
+-- BASIC PHASE 1B - INSERT seed data
+-- Your code here
