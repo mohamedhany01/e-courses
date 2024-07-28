10f72c12e94ad891b718bcc21f3da01f16889341
mohamedhany01 <mohamedhyouns@gmail.com>
2024-07-28 15:12:54 +0300

    Appacademy | Back-End | practice-for-week-12-jwt-exploration-short
    - Finish files

--- appacademy/aa-backend/security/practice-for-week-12-jwt-exploration-short-practice-main/app.js
+++ appacademy/aa-backend/security/practice-for-week-12-jwt-exploration-short-practice-main/app.js
@@ -3,14 +3,19 @@ require('dotenv').config();
 
 // Import package
 
-// Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
 // 1. Sign (create) a JWT containing your email address
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
-// Your code here
+token = jwt.sign(
+    { email: "johnny@gmail.com" },
+    process.env.SECRET_KEY,
+    // { expiresIn: '1h' }
+    { expiresIn: '1s' }
+);
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -19,7 +24,7 @@ console.log('JWT:', token);
 
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
-// Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -28,7 +33,7 @@ console.log('Payload:', payload);
 
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
-// Your code here
+verifiedPayload = jwt.verify(token, process.env.SECRET_KEY);
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
@@ -38,7 +43,14 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify
 //    Then "catch" the error and log it to the console.
 
-// Your code here
+try {
+    jwt.verify(token, process.env.INVALID_SECRET_KEY);
+} catch (error) {
+
+    const { name, message } = error;
+
+    console.log(`Error: ${name} ${message}`);
+}
 
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
@@ -46,4 +58,13 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify (with proper secret)
 //    Then "catch" the error and log it to the console
 
-// Your code here
+setTimeout(() => {
+    try {
+        jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+
+        const { name, message } = error;
+
+        console.log(`Error: ${name} ${message}`);
+    }
+}, 2000);
