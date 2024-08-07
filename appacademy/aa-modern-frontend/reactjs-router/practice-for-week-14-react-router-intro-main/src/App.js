8011637ac06f90dad268b2949ddae25309c03c1c
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-07 17:11:02 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-router-intro-main
    - Start files

---
+++ appacademy/aa-modern-frontend/reactjs-router/practice-for-week-14-react-router-intro-main/src/App.js
@@ -0,0 +1,16 @@
+import Home from './components/Home';
+import Stocks from './components/Stocks';
+import Movies from './components/Movies';
+
+function App() {
+  return (
+    <div className='main'>
+      <h1>App Component</h1>
+      <Home />
+      <Stocks />
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
