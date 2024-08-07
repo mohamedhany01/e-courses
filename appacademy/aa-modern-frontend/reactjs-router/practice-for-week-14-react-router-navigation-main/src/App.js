c42753a06e94e4d45d8a61aceadea192675ee451
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-07 18:14:20 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-router-navigation
    - Finish files

--- appacademy/aa-modern-frontend/reactjs-router/practice-for-week-14-react-router-navigation-main/src/App.js
+++ appacademy/aa-modern-frontend/reactjs-router/practice-for-week-14-react-router-navigation-main/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
@@ -7,6 +7,25 @@ function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav className='comp nav'>
+        <ul>
+          <li>
+            <NavLink activeStyle={{ fontWeight: 'bold' }} activeClassName='purple' exact={true} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={{ fontWeight: 'bold' }} activeClassName='purple' to="/stocks">
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={{ fontWeight: 'bold' }} activeClassName='purple' to="/movies">
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path='/'>
           <Home />
@@ -16,6 +35,9 @@ function App() {
         </Route>
         <Route path='/movies'>
           <Movies />
+        </Route>
+        <Route path='/not-logged-in'>
+          <h1> You Must Be Logged In To Enter.</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
