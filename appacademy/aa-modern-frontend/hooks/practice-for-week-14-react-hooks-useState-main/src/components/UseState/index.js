1f93066b3dcc48ced2a5f47e7b7adb992c7d0741
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-12 17:48:24 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-hooks-useState
    - Start files

---
+++ appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useState-main/src/components/UseState/index.js
@@ -0,0 +1,20 @@
+import './UseState.css';
+
+const UseState = () => {
+  return (
+    <div className="state">
+      <h1>UseState Component</h1>
+      <button>Dark</button>
+      <button>Light</button>
+      <h2>DISPLAY COUNT HERE</h2>
+      <button>
+        Increment
+      </button>
+      <button>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default UseState;
