572c66f6f4e97960c31738fb41875ce870a4bca7
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-12 18:14:19 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-hooks-useState
    - Finish files

--- appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useState-main/src/components/UseState/index.js
+++ appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useState-main/src/components/UseState/index.js
@@ -1,16 +1,22 @@
 import './UseState.css';
+import { useState } from 'react';
 
 const UseState = () => {
+  const [theme, setTheme] = useState('light');
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="state">
+    <div className={`state ${theme}`} >
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Toggle Theme</button>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('light')}>Light</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((prev) => prev + 1)}>
         Increment
       </button>
-      <button>
+      <button onClick={() => setCount((prev) => prev - 1)}>
         Decrement
       </button>
     </div>
