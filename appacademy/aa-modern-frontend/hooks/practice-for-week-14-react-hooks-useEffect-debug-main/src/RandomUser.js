5ed13858dab8dfdb6dfdea03e1621c983bfca800
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-13 17:49:56 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-hooks-useEffect-debug
    - Finish files

--- appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useEffect-debug-main/src/RandomUser.js
+++ appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useEffect-debug-main/src/RandomUser.js
@@ -8,21 +8,26 @@ const RandomUserTwo = () => {
   const [num, setNum] = useState(0);
   const [searchChange, setSearchChange] = useState('');
   const [searchWord, setSearchWord] = useState(
-    localStorage.setItem('user') || 'foobar'
+    localStorage.getItem('user') || 'foobar'
   );
   
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchUser = () => {
+    const fetchUser = async () => {
       const res = await fetch(`https://randomuser.me/api/?seed=${searchWord}`);
       const data = await res.json();
       setData(data.results);
     };
-  }, []);
 
-  useEffect(() => {
-    localStorage.getItem('user', searchWord);
+    const currentUser = localStorage.getItem('user');
+
+    if (!currentUser) {
+      fetchUser();
+    } else {
+      setSearchWord(currentUser);
+      fetchUser();
+    }
   }, [searchWord]);
 
   useEffect(() => {
@@ -30,6 +35,8 @@ const RandomUserTwo = () => {
       console.log('i am running');
       setNum((prevNum) => (prevNum === 3 ? 0 : prevNum + 1));
     }, 7000);
+
+    return () => (clearInterval(colorInterval));
   }, []);
 
   return (
@@ -50,6 +57,9 @@ const RandomUserTwo = () => {
           onSubmit={(e) => {
             e.preventDefault();
             setSearchWord(searchChange);
+            
+            localStorage.setItem('user', searchChange);
+
             setSearchChange('');
           }}
         >
--- appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useEffect-debug-main/src/Unmounted.js
+++ appacademy/aa-modern-frontend/hooks/practice-for-week-14-react-hooks-useEffect-debug-main/src/Unmounted.js
@@ -1,10 +1,13 @@
 import { useHistory } from 'react-router-dom';
 
 const Unmounted = () => {
+
+  const history = useHistory();
+
   return (
     <div className='unmounted'>
       <h1>Welcome</h1>
-      <button onClick={() => history.pus('/mount')}>Login</button>
+      <button onClick={() => history.push('/mount')}>Login</button>
     </div>
   );
 };
