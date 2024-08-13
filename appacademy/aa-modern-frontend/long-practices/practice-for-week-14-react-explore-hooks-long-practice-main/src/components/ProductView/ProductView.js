6ba3bc349c42fd54654b9e63f2c287f7a8cfa896
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-13 18:45:05 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-explore-hooks-long-practice
    - Start files

---
+++ appacademy/aa-modern-frontend/long-practices/practice-for-week-14-react-explore-hooks-long-practice-main/src/components/ProductView/ProductView.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ProductListItem from "../ProductListItem";
+import ProductDetails from "../ProductDetails";
+import './ProductView.css'
+
+function ProductView({ products }) {
+
+  // TODO: Replace with state variable
+  const sideOpen = true;
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map(item =>
+            <ProductListItem
+              key={item.id}
+              product={item}
+              onClick={() => console.log('SELECT PRODUCT', item)}
+            />
+          )}
+        </div>
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div className="product-side-panel-toggle"
+               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+            {sideOpen ? '>' : '<'}
+          </div>
+        </div>
+        <ProductDetails visible={sideOpen} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductView;
