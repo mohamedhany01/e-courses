3a1b519317c9f56876ff18160617805f34f3747a
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-16 15:12:35 +0300

    Appacademy | Modern Front-End | practice-for-week-14-react-context-pup-reading
    - Start files

---
+++ appacademy/aa-modern-frontend/context/practice-for-week-14-react-context-pup-reading-main/src/components/PupForm/PupForm.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import banana from '../../pups/banana-pup.jpg';
+import sleepy from '../../pups/sleepy-pup.jpg';
+import speedy from '../../pups/speedy-pup.jpg';
+
+function PupForm() {
+  const [selectedPup, setSelectedPup] = useState('select');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <select
+        name="pup"
+        onChange={e => setSelectedPup(e.target.value)}
+        value={selectedPup}
+      >
+        <option value="select" disabled>Select a pup!</option>
+        <option value={speedy}>Speedy Pup</option>
+        <option value={banana}>Banana Pup</option>
+        <option value={sleepy}>Sleepy Pup</option>
+      </select>
+      <button>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default PupForm;
---
+++ appacademy/aa-modern-frontend/context/practice-for-week-14-react-context-pup-reading-main/src/components/PupImage/PupImage.js
@@ -0,0 +1,11 @@
+import speedy from '../../pups/speedy-pup.jpg';
+import banana from '../../pups/banana-pup.jpg';
+import sleepy from '../../pups/sleepy-pup.jpg';
+
+const PupImage = () => {
+  return (
+    <img src={speedy} alt="pup" />
+  );
+};
+
+export default PupImage;
