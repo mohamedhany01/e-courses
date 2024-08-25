01e1a3d327e1cd427167a487a39d1e0a3063130e
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-25 18:52:12 +0300

    Appacademy | Modern Front-End | practice-for-week-15-react-redux-useDispatch-useSelector
    - Finish files

--- appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-useDispatch-useSelector-main/src/components/ArticleList/index.js
+++ appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-useDispatch-useSelector-main/src/components/ArticleList/index.js
@@ -1,16 +1,27 @@
 import { Route, Switch } from 'react-router-dom';
 import SingleArticle from '../SingleArticle';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { loadArticles } from '../../store/articleReducer';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
 
 const ArticleList = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadArticles())
+  }, [dispatch]);
+
+  const articles = useSelector(state => state.articleState.entries);
+
   return (
     <div>
       <h1>Article List</h1>
       <ol>
-        <li>Gilligan's Island. Is it true?</li>
-        <li>A Baseball Moment</li>
-        <li>Poke Moment</li>
-        <li>Cool Cats</li>
-        <li>Why Am I At Home</li>
+        {articles.map(({ id, title }) => (
+          <li key={id}><NavLink to={`/article/${id}`}>{title}</NavLink></li>
+        ))}
       </ol>
 
       <Switch>
