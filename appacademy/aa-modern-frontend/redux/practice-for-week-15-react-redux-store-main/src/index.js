8ce6a9871184c34f1fe62997cb254c1884840832
mohamedhany01 <mohamedhyouns@gmail.com>
2024-08-25 16:40:59 +0300

    Appacademy | Modern Front-End | practice-for-week-15-react-redux-store
    - Finish files

--- appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-store-main/src/index.js
+++ appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-store-main/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+
+import configureStore from './store';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-store-main/src/store/index.js
+++ appacademy/aa-modern-frontend/redux/practice-for-week-15-react-redux-store-main/src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import fruitReducer from './fruitReducer';
 
 // `combineReducers` combines all the reducer functions into one big reducer
 // function, which is typically called `rootReducer`. This is the most important
 // part of this file. You will add your reducers here to work with your various
 // components.
 
-// create rootReducer here:
+// create rootReducer:
+const rootReducer = combineReducers({
+  fruitState: fruitReducer
+});
 
 // `enhancer` allows you to alter the store and add functionality such as the
 // Redux DevTools and logger (similar to morgan) middleware
