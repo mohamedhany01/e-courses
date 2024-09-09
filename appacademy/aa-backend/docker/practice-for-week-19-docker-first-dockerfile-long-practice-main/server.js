88ef561b5d976a7acc36098307605f0e648bd365
mohamedhany01 <mohamedhyouns@gmail.com>
2024-09-09 10:43:27 +0300

    Appacademy | Back-End | practice-for-week-19-docker-first-dockerfile-long
    - Finish files

---
+++ appacademy/aa-backend/docker/practice-for-week-19-docker-first-dockerfile-long-practice-main/server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http
+    .createServer(function (req, res) {
+        res.write("Hello World! It's good to be here.");
+        res.end();
+    })
+    .listen(8000);
