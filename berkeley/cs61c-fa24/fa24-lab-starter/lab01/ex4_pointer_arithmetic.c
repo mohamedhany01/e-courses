e1d3e6ff43800042fb56100eef14f4d7cee797b8
mohamedhany01 <61619208+mohamedhany01@users.noreply.github.com>
2025-01-14 22:31:49 +0200

    CS61C | lab 01 | ex4

--- berkeley/cs61c-fa24/fa24-lab-starter/lab01/ex4_pointer_arithmetic.c
+++ berkeley/cs61c-fa24/fa24-lab-starter/lab01/ex4_pointer_arithmetic.c
@@ -8,20 +8,21 @@ int main() {
   int16_t x = 5;
 
   // TODO: create an int16_t array of size 4
-  ______ some_array[______];
+  const int SIZE = 4;
+  int16_t some_array[SIZE];
   printf("address of the start of the array: %p\n", some_array);
 
   // TODO: compute the address of the element at index 2 (0-indexed)
-  ______ ptr_to_idx_2 = ____________;
+  int16_t* ptr_to_idx_2 = some_array + 2;
   printf("address of index 2: %p\n", ptr_to_idx_2);
 
   // TODO: store the value 10 at index 2, using ptr_to_idx_2
-  __________ = 10;
+  *ptr_to_idx_2 = 10;
 
   // TODO: print the value at index 2
   // Hint: this blank should be the same as the previous blank
   //       please don't hard code 10
-  printf("value at index 2: %d\n", __________);
+  printf("value at index 2: %d\n", *ptr_to_idx_2);
 
   return 0;
 }
